3d43b4855c224c0d76ec9c82ab6c0e8efe31c98a
VidwathNK <vidwathkaranth@gmail.com>
2026-06-20 23:53:25 +0530

    Limit academic credits select options to max 4 across onboarding views and resources vault

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -389,8 +389,6 @@ export default function OnboardingPage() {
                             <option value="2">2</option>
                             <option value="3">3</option>
                             <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
                           </select>
                         </div>
                       </div>
--- src/components/OnboardingModal.tsx
+++ src/components/OnboardingModal.tsx
@@ -398,8 +398,6 @@ export default function OnboardingModal() {
                             <option value="2">2</option>
                             <option value="3">3</option>
                             <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
                           </select>
                         </div>
                       </div>
